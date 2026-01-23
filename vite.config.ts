340e6d7c7a68e45d0c26fc30024c48ba7d49486e
Eero Oinas <eero.oinas@etteplan.com>
2026-01-23 17:02:05 +0200

    Add chunking strategy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,29 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            {
+              name: 'react',
+              test: /[\\/]node_modules[\\/]react(-dom)?[\\/]/,
+            },
+            {
+              name: 'mui',
+              test: /[\\/]node_modules[\\/](@mui|@emotion)[\\/]/,
+            },
+            {
+              name: 'tanstack',
+              test: /[\\/]node_modules[\\/]@tanstack[\\/]/,
+            },
+          ],
+        },
+      },
+    },
+  },
 })
